b3bc7545f0f82f0cf0fe55c267ef05a4ea7c9eb5
irerimwenda <bmugendi98@gmail.com>
2019-10-02 16:53:01 +0300

    Validation

--- app/Http/Controllers/API/UserController.php
+++ app/Http/Controllers/API/UserController.php
@@ -29,7 +29,14 @@ class UserController extends Controller
      */
     public function store(Request $request)
     {
-        //Add User to DB
+        // Validate
+        $this->validate($request, [
+            'name' => 'required|string|max:191',
+            'email' => 'required|string|email|max:191|unique:users',
+            'name' => 'required|min:8',
+        ]);
+
+        // Add User to DB
         return User::create([
             'name' => $request['name'],
             'email' => $request['email'],
