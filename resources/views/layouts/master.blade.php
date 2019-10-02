d49fc62e479b96c86c486d1f06fabcc6eaa549b9
irerimwenda <bmugendi98@gmail.com>
2019-10-02 13:02:10 +0300

    HTML5 history mode - vue router

--- resources/views/layouts/master.blade.php
+++ resources/views/layouts/master.blade.php
@@ -79,9 +79,9 @@ scratch. This page gets rid of all links and provides the needed markup only.
               </p>
             </router-link>
           </li>
-
+ 
           <li class="nav-item has-treeview ">
-            <a href="#" class="nav-link active">
+            <a href="#" class="nav-link">
               <i class="nav-icon fas fa-cog"></i>
               <p>
                 Managamement
@@ -90,7 +90,7 @@ scratch. This page gets rid of all links and provides the needed markup only.
             </a>
             <ul class="nav nav-treeview">
               <li class="nav-item">
-                <a href="#" class="nav-link active">
+                <a href="#" class="nav-link">
                   <i class="far fa-circle nav-icon"></i>
                   <p>Active Page</p>
                 </a>
