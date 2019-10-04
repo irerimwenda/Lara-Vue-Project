60d41d3cb8efaebfa5b3f5e604984e3ab1345a66
irerimwenda <bmugendi98@gmail.com>
2019-10-04 14:30:23 +0300

    auth:api -> secure login data

--- app/Http/Controllers/API/UserController.php
+++ app/Http/Controllers/API/UserController.php
@@ -12,6 +12,16 @@ use App\user;
 class UserController extends Controller
 {
     /**
+     * Create a new controller instance.
+     *
+     * @return void
+     */
+    public function __construct()
+    {
+        $this->middleware('auth:api');
+    }
+
+    /**
      * Display a listing of the resource.
      *
      * @return \Illuminate\Http\Response
