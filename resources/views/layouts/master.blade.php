99e4a7a6e370e6af7ecb0e843df1a99a6bdce361
irerimwenda <bmugendi98@gmail.com>
2019-10-05 10:45:28 +0300

    user profile component

--- resources/views/layouts/master.blade.php
+++ resources/views/layouts/master.blade.php
@@ -13,7 +13,7 @@ scratch. This page gets rid of all links and provides the needed markup only.
   <!-- CSRF Token -->
   <meta name="csrf-token" content="{{ csrf_token() }}">
 
-  <title>AdminLTE 3 | Starter</title>
+  <title>Lara-Vue | Admin</title>
 
   <link rel="stylesheet" href="{{ asset('css/app.css') }}">
 
